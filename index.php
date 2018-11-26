eff3823eb5c01118398fec3c37dbd8ba49f4c417
John Rowley <outsidethebox@eircom.net>
2018-11-26 16:50:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
 
-echo "Hello DBS, it is Monday. From John R."
+echo "Hello DBS, it is Monday. From John R.";
+
+for($i = 0; $i < 10; $i++) {
+  echo "<p>This is a new paragraphp $i</p>";
+  
+}
+  
 
 ?>
